c0abe2455410feb90e745c9863f16d874ab98770
SadiqOyo <mohammad.sadique@oyorooms.com>
2021-06-09 19:17:15 +0530

    setup

---
+++ src/main/java/com/oyo/oyoExt/OyoExtApplication.java
@@ -0,0 +1,13 @@
+package com.oyo.oyoExt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OyoExtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OyoExtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oyo/oyoExt/OyoExtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oyo.oyoExt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OyoExtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
