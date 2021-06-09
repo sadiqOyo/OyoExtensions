58c17febd285ed85e9baed38ca1daef66022aad2
SadiqOyo <mohammad.sadique@oyorooms.com>
2021-06-09 21:50:51 +0530

    Entity added

--- src/main/java/com/oyo/oyoExt/Entities/OrderEntity.java
+++ src/main/java/com/oyo/oyoExt/Entities/OrderEntity.java
@@ -10,33 +10,53 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.Type;
+import org.hibernate.annotations.UpdateTimestamp;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Transient;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
 @Getter
 @Setter
 @Entity
-@Table(name = "order")
+@Table(name = "orders")
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class OrderEntity {
 
+    private final static String DATE_PATTERN = "yyyy-MM-dd HH:mm:ss Z";
+    @Transient
+    private final SimpleDateFormat sdf = new SimpleDateFormat(DATE_PATTERN);
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String booking_id;
+
     private String order_id;
+
     private String user_profile_id;
+
     private Boolean is_paid;
 
-    @Type(type = "JSONBUserType")
-    private JsonObject products;
+    private Products products;
+
+    @Column(nullable = false)
+    private Date createdAt;
+
+    @UpdateTimestamp
+    @Column(nullable = false)
+    private Date updatedAt;
+
 
 
 
--- src/main/java/com/oyo/oyoExt/Request/Products.java
+++ src/main/java/com/oyo/oyoExt/Request/Products.java
@@ -7,12 +7,14 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.io.Serializable;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class Products {
+public class Products implements Serializable {
     private String description;
     private String name;
     private Long price;
